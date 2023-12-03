b1b81d698b5fadac1a54b72e876b7dacfa8dec15
Kayley Wang <kw102@wellesley.edu>
2023-12-03 18:21:18 -0500

    set up our own Firebase sotrage and added in configuration code

--- components/App.js
+++ components/App.js
@@ -9,6 +9,19 @@ import { // access to Firestore features:
   getFirestore, 
 } from "firebase/firestore";
 
+// Your web app's Firebase configuration
+//For our StudyBuddies Firebase Team Account registration info
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAZdD7DvAhCsyuytcIYMcsCLS9SBe53g7Q",
+//   authDomain: "study-buddies-cs317.firebaseapp.com",
+//   projectId: "study-buddies-cs317",
+//   storageBucket: "study-buddies-cs317.appspot.com",
+//   messagingSenderId: "859373613647",
+//   appId: "1:859373613647:web:32e75334a39479b5a3b03a",
+//   measurementId: "G-Q77N8LXH92"
+// };
+
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 const auth = getAuth(firebaseApp);
